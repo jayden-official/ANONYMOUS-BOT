a9e09c98fcbb57a93d02836149c662e1b127ae09
Jayden-official <pancafe198@gmail.com>
2024-07-15 14:52:21 +0300

    Update maker-simpcard.js

--- plugins/maker-simpcard.js
+++ plugins/maker-simpcard.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/simpcard', {
     avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-    }), 'error.png', '*🤣🫵THIS DUDE IS THE BEST SIMP EVER🫵🤣*', m)
+    }), 'error.png', '*🤣🫵THIS DUDE IS THE BEST SIMPER EVER🫵🤣*', m)
     }
     handler.help = ['simpcard']
     handler.tags = ['maker']  
